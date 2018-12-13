9b72bf31934c7fcd8f06a5e4e4d5ae9e2f87fbd3
Orlando Castillo <omaddoc@gmail.com>
2018-12-12 23:52:19 -0500

    start5

--- api/server.js
+++ api/server.js
@@ -2,93 +2,68 @@ const express = require('express');
 const helmet = require('helmet');
 
 const knex = require('../db/config.js');
-const port = process.env.PORT || 9000;
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.send(`Api running on port: ${port}`);
+server.get('/api/notes', async (req, res) => {
+  knex('notes')
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => res.status(500).json(err));
 });
 
-server.get('/items', async (req, res) => {
-  try {
-    const items = await knex('items');
+server.post('/api/notes', (req, res) => {
+  const note = req.body;
 
-    res.status(200).json(items);
-  } catch (error) {
-    res.status(500).json({ message: 'error getting items from db' });
-  }
+  knex('notes')
+    .insert(note)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Error creating note' });
+    });
 });
 
-server.post('/items', async (req, res) => {
-  console.log(req.body);
-  try {
-    const result = await knex('items').insert(req.body, 'id');
+server.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
 
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(500).json({ message: 'error saving the item', error });
-  }
+  knex('notes')
+    .where({ id: id })
+    .then(note => {
+      res.status(200).json({ note });
+    })
+    .catch(err => res.status(500).json(err));
 });
 
-// server.get('/api/notes', async (req, res) => {
-//   knex('notes')
-//     .then(notes => {
-//       res.status(200).json(notes);
-//     })
-//     .catch(err => res.status(500).json(err));
-// });
+server.put('/api/notes/:id', (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
 
-// server.post('/api/notes', (req, res) => {
-//   const note = req.body;
+  knex('notes')
+    .where({ id: id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
 
-//   knex('notes')
-//     .insert(note)
-//     .returning('id')
-//     .then(ids => {
-//       res.status(201).json(ids);
-//     })
-//     .catch(err => {
-//       res.status(500).json({ error: 'Error creating note' });
-//     });
-// });
+server.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
 
-// server.get('/api/notes/:id', (req, res) => {
-//   const { id } = req.params;
-
-//   knex('notes')
-//     .where({ id: id })
-//     .then(note => {
-//       res.status(200).json({ note });
-//     })
-//     .catch(err => res.status(500).json(err));
-// });
-
-// server.put('/api/notes/:id', (req, res) => {
-//   const changes = req.body;
-//   const { id } = req.params;
-
-//   knex('notes')
-//     .where({ id: id })
-//     .update(changes)
-//     .then(count => {
-//       res.status(200).json({ count });
-//     })
-//     .catch(err => res.status(500).json(err));
-// });
-
-// server.delete('/api/notes/:id', (req, res) => {
-//   const { id } = req.params;
-
-//   knex('notes')
-//     .where({ id: id })
-//     .del()
-//     .then(count => {
-//       res.status(200).json({ count });
-//     })
-//     .catch(err => res.status(500).json(err));
-// });
+  knex('notes')
+    .where({ id: id })
+    .del()
+    .then(count => {
+      res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 module.exports = server;
